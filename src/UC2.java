eee80d34be73762d4e2c183ecc18759398c1c63c
HarshithaGannavarapu04057 <harshithagannavarapu71@gmail.com>
2026-02-23 10:21:26 +0530

    UC2:Hardcoded palindrome

--- src/UC2.java
+++ src/UC2.java
@@ -1,1 +1,19 @@
-class Palindro
+class UC2PalindromeCheckerApp {
+    public static void main(String[] args){
+        String word = "madam";
+     System.out.println("Palindrome Checker App - UC2");
+     System.out.println("------------------------------");
+     System.out.println("Hardcoded string: "  + word);
+        String reversed = "";
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+        if (word.equals(reversed)) {
+            System.out.println("Result: The  string is a PALINDROME.");
+        } else {
+            System.out.println("Result: The given string is NOT a palindrome.");
+        }
+
+        System.out.println("\nProgram executed successfully.");
+    }
+}
