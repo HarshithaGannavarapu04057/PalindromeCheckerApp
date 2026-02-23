bb9f1f8b4939721de999e84ff28f455d6e11859c
HarshithaGannavarapu04057 <harshithagannavarapu71@gmail.com>
2026-02-23 09:25:17 +0530

    Base application setup

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import javax.xml.transform.Source;
+import java.util.*;
+
+class PalindromeCheckerApp {
+    static void main() {
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully.");
+    }
+}
