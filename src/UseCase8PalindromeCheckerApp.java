0513560dca344f537b5ad096f6ea41ccba91af15
HarshithaGannavarapu04057 <harshithagannavarapu71@gmail.com>
2026-03-02 09:57:04 +0530

    UC8:LinkedList Based PalindromeChecker

---
+++ src/UseCase8PalindromeCheckerApp.java
@@ -0,0 +1,24 @@
+import java.util.LinkedList;
+public class UseCase8PalindromeCheckerApp {
+public static void main(String[] args) {
+String input = "level";
+LinkedList<Character> list = new LinkedList<>();
+for (char c : input.toCharArray()) {
+list.add(c);
+ }
+boolean isPalindrome = true;
+while (list.size() > 1) {
+char first = list.removeFirst();
+char last = list.removeLast();
+if (first != last) {
+isPalindrome = false;
+break;
+}
+ }
+ if (isPalindrome) {
+ System.out.println(input + " is a palindrome.");
+} else {
+ System.out.println(input + " is not a palindrome.");
+ }
+ }
+}
